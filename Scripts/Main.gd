334ec302d9955fed8e60afa0bf93ecdafebddc91
Dinis Costa <scorpaust@hotmail.com>
2021-06-17 18:55:01 +0100

    Game system adjustments and SFX system.

---
+++ Scripts/Main.gd
@@ -0,0 +1,82 @@
+extends Node
+
+const new_spider = preload("res://Scenes/Spider.tscn")
+
+const new_flower = preload("res://Scenes/Flower.tscn")
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var pts = 0
+
+var lifes = 3
+
+var gameover = false
+
+signal damaged
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("BackgroundMusic").play()
+	pass # Replace with function body.
+
+
+func spawn_spider():
+	var spider = new_spider.instance()
+	spider.position.x = int(rand_range(0, 720))
+	spider.position.y = 0
+	get_node("Container").add_child(spider)
+	pass
+
+func spawn_flower():
+	var flower = new_flower.instance()
+	flower.position.x = int(rand_range(0, 720))
+	flower.position.y = 0
+	get_node("Container").add_child(flower)
+	pass
+
+func _on_Timer_Spider_timeout():
+	if gameover == false:
+		spawn_spider()
+
+
+func _on_Timer_Flower_timeout():
+	if gameover == false:
+		spawn_flower()
+
+
+func _on_Area_Bee_area_entered(area):
+	if area.name == "Area_Spider":
+		
+		emit_signal("damaged")
+		
+		if (lifes == 3):
+			lifes -= 1
+			get_node("Bar/3").hide()
+		
+		elif (lifes == 2):
+			lifes -= 1
+			get_node("Bar/2").hide()
+			
+		elif (lifes == 1):
+			lifes -= 1
+			gameover = true
+			get_node("BackgroundMusic").stop()
+			get_node("Bar/3").hide()
+			get_node("Bee").queue_free()
+			get_node("GameOver").show()
+			get_node("Restart").start()
+			
+		
+	if area.name == "Area_Flower":
+		pts += 1
+		get_node("PickupSFX").play()
+		get_node("Bar/Score").text = str(pts)
+		area.get_parent().queue_free()
+
+
+
+func _on_Restart_timeout():
+	get_tree().reload_current_scene()
+	pass # Replace with function body.
